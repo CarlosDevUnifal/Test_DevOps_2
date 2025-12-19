b3c574e4b73f267237c6bb5ba8a4c7280f18fa80
CarlosDevUnifal <carlos.arantes@sou.unifal-mg.edu.br>
2025-12-19 09:46:44 -0300

    resolvendo -Wclang-format-violations

--- calculator/src/main.cpp
+++ calculator/src/main.cpp
@@ -4,10 +4,10 @@
 
 #include "calculator.hpp"
 
-int main()
-{
+int main() {
     double number1 = 0.0;
     double number2 = 0.0;
+
     std::cout << "Hello Calculator" << std::endl;
     std::cout << "Enter Number 1: ";
     std::cin >> number1;
@@ -16,7 +16,8 @@ int main()
 
     Calculator<double> c(number1, number2);
 
-    std::cout << "Result -> " << number1 << " / " << number2 << " = " << c.divide() << std::endl;
+    std::cout << "Result -> " << number1 << " / " << number2 << " = "
+                        << c.divide() << std::endl;
 
     return 0;
 }
