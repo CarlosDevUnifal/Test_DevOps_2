dc81879328fe33e79aa80a1c73f992ed8baeffac
CarlosDevUnifal <carlos.arantes@sou.unifal-mg.edu.br>
2025-12-19 09:58:37 -0300

    resolvendo warning [-Wclang-format-violations]

--- calculator/src/main.cpp
+++ calculator/src/main.cpp
@@ -5,21 +5,21 @@
 #include "calculator.hpp"
 
 int main() {
-    double number1 = 0.0;
-    double number2 = 0.0;
+  double number1 = 0.0;
+  double number2 = 0.0;
 
-    std::cout << "Hello Calculator" << std::endl;
-    std::cout << "Enter Number 1: ";
-    std::cin >> number1;
-    std::cout << "Enter Number 2: ";
-    std::cin >> number2;
+  std::cout << "Hello Calculator" << std::endl;
+  std::cout << "Enter Number 1: ";
+  std::cin >> number1;
+  std::cout << "Enter Number 2: ";
+  std::cin >> number2;
 
-    Calculator<double> c(number1, number2);
+  Calculator<double> c(number1, number2);
 
-    std::cout << "Result -> " << number1 << " / " << number2 << " = "
-                        << c.divide() << std::endl;
+  std::cout << "Result -> " << number1 << " / " << number2 << " = "
+            << c.divide() << std::endl;
 
-    return 0;
+  return 0;
 }
 
 #endif
